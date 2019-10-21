6a2b8da391b34f1cf36e8cf30e991ad9b3831190
LeagueLugas <48398942+LeagueLugas@users.noreply.github.com>
2019-10-21 17:26:41 +0900

    Update School IP.bat

--- School IP.bat
+++ School IP.bat
@@ -1,1 +1,1 @@
-netsh -c int ip set address name="Wi-Fi" source=static addr=10.156.147.149 mask=255.255.255.0 gateway=10.156.147.1 gwmetric=0
+netsh -c int ip set address name="Wi-Fi" source=static addr=자신의아이피 mask=255.255.255.0 gateway=10.156.147.1 gwmetric=0
